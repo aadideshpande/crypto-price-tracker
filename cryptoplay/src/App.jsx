2831aa53afe004f858919242668d749eb191bfed
aadideshpande <aadi.deshpande1@gmail.com>
2021-08-02 16:50:02 +0530

    added context to get previously stored data

--- cryptoplay/src/App.jsx
+++ cryptoplay/src/App.jsx
@@ -4,13 +4,16 @@ import CoinSummary from './pages/CoinSummary';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import "./App.css"
+import { WatchListContextProvider } from './context/watchListContext';
 const App = () => {
     return (
         <div>
-           <BrowserRouter>
-                <Header />
-                <Route exact path = "/" component = {CoinSummary} />
-           </BrowserRouter>
+            <WatchListContextProvider>
+                <BrowserRouter>
+                    <Header />
+                    <Route exact path="/" component={CoinSummary} />
+                </BrowserRouter>
+            </WatchListContextProvider>
         </div>
     )
 }
