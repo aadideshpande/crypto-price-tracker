1f1ac7b2d81c2133295813964e2c5deb4deb0215
aadideshpande <aadi.deshpande1@gmail.com>
2021-08-07 17:52:48 +0530

    charts introduced

--- cryptoplay/src/components/HistoryChart.jsx
+++ cryptoplay/src/components/HistoryChart.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import { Chart } from "chart.js";
+import { historyOptions } from '../chartConfigs/chartConfigs';
 
-const HistoryChart = () => {
+const HistoryChart = ({ data }) => {
+    const chartRef = useRef()
+    const { day, week, year, detail } = data;
+    useEffect(() => {
+        if (chartRef && chartRef.current) {
+            const chartInstance = new Chart(chartRef.current, {
+                type: 'line',
+                data: {
+                    datasets: [{
+                        data: day,
+                        backgroundColor:
+                            'rgba(255, 99, 132, 0.2)',
+                        borderColor:
+                            'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: historyOptions
+            });
+        }
+    }, [])
     return (
-        <div>
-            
+        <div className="bg-white border mt-2 rounded p-3">
+            <div>
+                <canvas ref={chartRef} id="myChart" width={250} height={250} />
+            </div>
         </div>
     )
 }
--- cryptoplay/src/pages/CoinDetails.jsx
+++ cryptoplay/src/pages/CoinDetails.jsx
@@ -4,48 +4,74 @@ import CoinData from '../components/CoinData';
 import HistoryChart from '../components/HistoryChart';
 import coinGecko from '../apis/coinGecko';
 const CoinDetails = () => {
-    const {coin} = useParams();
+    const { coin } = useParams();
     const [coinData, setCoinData] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const formatData = data => {
+        return data.map(el => {
+            return {
+                x: el[0],
+                y: el[1].toFixed(2)
+            }
+        });
+    }
+
     useEffect(() => {
         const fetchData = async () => {
-            const resultsDay = await coinGecko.get(
-                `/coins/${coin}/market_chart/`, {
+            setIsLoading(true);
+            const [day, week, year, detail] = await Promise.all([
+                coinGecko.get(
+                    `/coins/${coin}/market_chart/`, {
                     params: {
                         vs_currency: "usd",
                         days: "1"
                     }
-                }
-            )
-            const resultsWeek = await coinGecko.get(
-                `/coins/${coin}/market_chart/`, {
+                }),
+                coinGecko.get(
+                    `/coins/${coin}/market_chart/`, {
                     params: {
                         vs_currency: "usd",
                         days: "7"
                     }
-                }
-            )
-            const resultsYear = await coinGecko.get(
-                `/coins/${coin}/market_chart/`, {
+                }),
+                coinGecko.get(
+                    `/coins/${coin}/market_chart/`, {
                     params: {
                         vs_currency: "usd",
                         days: "365"
                     }
-                }
-            )
+                }),
+                coinGecko.get("/coins/markets", {
+                    params: {
+                        vs_currency: "usd",
+                        ids: coin
+                    }
+                })
+            ]);
             setCoinData({
-                day: resultsDay.data.prices,
-                week: resultsWeek.data.prices,
-                year: resultsYear.data.prices
+                day: formatData(day.data.prices),
+                week: formatData(week.data.prices),
+                year: formatData(year.data.prices),
+                detail: detail.data[0]
             });
+            setIsLoading(false);
         }
         fetchData();
     }, [])
-    return (
-        <div className="coinlist">
-            <HistoryChart />
-            <CoinData />
-        </div>
-    )
+    const renderData = () => {
+        if (isLoading) {
+            return <div>Loading</div>
+        } else {
+            return (
+                <div className="coinlist">
+                    <HistoryChart data={coinData} />
+                    <CoinData />
+                </div>
+            )
+        }
+    }
+    return renderData();
 }
 
 export default CoinDetails
