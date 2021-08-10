dacfc3e341f013edac8949f8a7401257b20d04c4
aadideshpande <aadi.deshpande1@gmail.com>
2021-08-10 11:11:48 +0530

    dropdown to append new coins added

--- cryptoplay/src/components/AddCoin.jsx
+++ cryptoplay/src/components/AddCoin.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddCoin = () => {
+    const availableCoins = [
+        "bitcoin",
+        "ethereum",
+        "litecoin",
+        "dogecoin",
+        "eos",
+        "tezos"
+    ];
+    const [showDropDown, setShowDropDown] = useState(false);
     return (
-        <div>
-            
+        <div className="dropdown">
+            <button
+                onClick={() => setShowDropDown(!showDropDown)}
+                className="btn btn-primary dropdown-toggle"
+                type="button">
+                Add Coin
+            </button>
+            <div className={showDropDown ? "dropdown-menu show" :
+                "dropdown-menu"}>
+                {availableCoins.map(el => {
+                    return (
+                        <a href="#" className="dropdown-item">
+                            {el}
+                        </a>
+                    )
+                })}
+            </div>
         </div>
     )
 }
